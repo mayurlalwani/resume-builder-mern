17148378926714a52e5b7e868016067f3c77cdbe
mayurlalwani <lalwanimayur06@gmail.com>
2022-04-19 17:38:25 +0530

    modify db configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,9 +5,6 @@ const connectDB = async () => {
     const con = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      //   useCreateIndex: true,
     });
     console.log(`MongoDB connected:${con.connection.host}`);
   } catch (error) {
